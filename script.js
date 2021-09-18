d3783f2f8f9b2ab227ad85de4852d702b739a715
Yuriy-Plakhin <yuriy.plakhin@gmail.com>
2021-09-18 20:13:29 +0600

    Implemented main text appearing slow animation

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 'use-strict';
 
+const catchWord =
+  'If life has taught me one thing, it’s that there are no villains. Only people, doing their best.';
+const nameOfAuthor = 'Joe Abercrombie';
+
+const textShowSpeed = 2000;
+const authorSignShowDelay = 1000;
+
 const navPanel = document.querySelector('.nav--panel');
+const introductionQuote = document.querySelector('.introduction--quote');
+const authorSign = document.querySelector('.abercrombie--ref');
 
 const scrollToSection = function (btn, section) {
   btn.addEventListener('click', function (e) {
@@ -8,6 +17,48 @@ const scrollToSection = function (btn, section) {
     section.scrollIntoView({ behavior: 'smooth', block: 'center' });
   });
 };
+
+navPanel.insertAdjacentHTML;
+
+// const addClassWithDelay = function (insertEl, targetEl, delay = 0) {
+//   [...insertEl].map(function (letter, i) {
+//     setTimeout(function () {
+//       targetEl.insertAdjacentHTML(
+//         'beforeEnd',
+//         `<strong class='show--element'>${letter}</strong>`
+//       );
+//     }, i * delay);
+//   });
+
+const addTextAndShowItWithDelay = function (
+  insertEl,
+  targetEl,
+  delay = 0,
+  customClass = 'strong'
+) {
+  [...insertEl].map(function (letter, i) {
+    const id = `${customClass}--${i + 1}`;
+
+    targetEl.insertAdjacentHTML(
+      'beforeEnd',
+      `<strong class='hide--element ${customClass} ${id}'>${letter}</strong>`
+    );
+    setTimeout(function () {
+      const letter = document.querySelector(`.${id}`);
+      letter.classList.remove('hide--element');
+    }, i * delay);
+  });
+};
+
+addTextAndShowItWithDelay(
+  catchWord,
+  introductionQuote,
+  textShowSpeed / catchWord.length,
+  'catchWord'
+);
+setTimeout(function () {
+  addTextAndShowItWithDelay(nameOfAuthor, authorSign, 50, 'abercrombie--ref');
+}, textShowSpeed + authorSignShowDelay);
 
 navPanel.addEventListener('click', function (e) {
   e.preventDefault();
