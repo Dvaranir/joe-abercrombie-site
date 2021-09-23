0232d561e9585041542e016cfcf38da6457fded2
Yuriy-Plakhin <yuriy.plakhin@gmail.com>
2021-09-23 16:41:55 +0600

    Comments added

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const nameOfAuthor = 'Joe Abercrombie';
 const textShowSpeed = 5000;
 const authorSignShowDelay = 1000;
 
+// All Selected Items
 const header = document.querySelector('#header');
 const goTopBtnAnchor = document.querySelector('.go--top');
 const goTopBtn = document.querySelector('.go--top__btn');
@@ -27,6 +28,7 @@ const carouselElements = document.querySelectorAll('.book--card');
 const sliderRightArrow = document.querySelector('.slider--right__arrow');
 const sliderLeftArrow = document.querySelector('.slider--left__arrow');
 
+// Smooth scrolling to sections
 const scrollToSection = function (btn, section) {
   btn.addEventListener('click', function (e) {
     e.preventDefault();
@@ -34,6 +36,7 @@ const scrollToSection = function (btn, section) {
   });
 };
 
+// Main image appearing text function
 const addTextAndShowItWithDelay = function (
   insertEl,
   targetEl,
@@ -54,7 +57,8 @@ const addTextAndShowItWithDelay = function (
   });
 };
 
-const displayMainImageTextText = function () {
+// Combined appearing text function and settimeout to animate text appearing, also adding additional delay to author sign appearing
+const displayMainImageText = function () {
   addTextAndShowItWithDelay(
     catchWord,
     introductionQuote,
@@ -66,6 +70,7 @@ const displayMainImageTextText = function () {
   }, textShowSpeed + authorSignShowDelay);
 };
 
+// Adding event to Nav buttons from there's parent
 navPanel.addEventListener('click', function (e) {
   e.preventDefault();
   if (!e.target.classList.contains('nav--btn')) return;
@@ -75,7 +80,8 @@ navPanel.addEventListener('click', function (e) {
     .scrollIntoView({ behavior: 'smooth', block: 'center' });
 });
 
-const showLLiftButton = function (entries, observer) {
+// Showing lift-arrow button which smooth scrolling to nav when clicked
+const showLLiftButton = function (entries) {
   const [entry] = entries;
 
   if (!entry.isIntersecting) {
@@ -92,6 +98,7 @@ const introSectionObserver = new IntersectionObserver(showLLiftButton, {
 
 introSectionObserver.observe(introSection);
 
+// Animating appearing of sections on scroll
 const showHiddenSection = function (entries, observer) {
   const [entry] = entries;
   if (!entry.isIntersecting) return;
@@ -110,6 +117,7 @@ allSections.forEach(section => {
   hiddenElementObserver.observe(section);
 });
 
+// Variables for slider
 let curSlide = 0;
 let maxSlides = carouselElements.length - 1;
 
@@ -141,11 +149,13 @@ const removeClassFromElements = function (elements, cls) {
   });
 };
 
+// Removing class that hides elements on specific window width
 const removeClassOnWindowWidth = function (windowWidth, elements, cls) {
   if (window.innerWidth > windowWidth) return;
   removeClassFromElements(elements, cls);
 };
 
+// Scroll top on reload
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 };
@@ -158,5 +168,5 @@ carouselElements.forEach((el, i) => {
 });
 
 scrollToSection(goTopBtnAnchor, header);
-document.addEventListener('DOMContentLoaded', displayMainImageTextText);
+document.addEventListener('DOMContentLoaded', displayMainImageText);
 removeClassOnWindowWidth(735, allHiddenSections, 'section--move--down__hide');
